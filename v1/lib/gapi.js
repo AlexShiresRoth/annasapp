2382988d490c69d53026466e7e332a94f7b6082d
Alex Roth <alexroth96@gmail.com>
2018-10-21 21:43:34 +0000

    update ui and js files

---
+++ v1/lib/gapi.js
@@ -0,0 +1,14 @@
+/*const googleapis = require('googleapis'),
+      OAuth2Client = googleapis.auth.OAuth2,
+      client = process.env.CLIENT_ID,
+      secret = process.env.CLIENT_SECRET,
+      redirect = 'https://localhost:3000/oauth2callback',
+      oauth2Client = new OAuth2Client(client, secret, redirect),
+      scopes = ['https://www.googleapis.com/auth/plus.me',
+      'https://www.googleapis.com/auth/photoslibrary.readonly'],
+      photos_auth_url = oauth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scopes: scopes
+      });
+
+googleapis.options({auth: oauth2Client});*/
--- v1/public/javascripts/data.js
+++ v1/public/javascripts/data.js
@@ -1,24 +1,45 @@
 
-let button = document.querySelector('button');
+let button = document.querySelector('.button-size');
+let newPhoto = document.querySelector('.button-img');
 let url = 'https://photos.app.goo.gl/MzpwHcFA6mjKn8ci1';
 let displayImg = document.querySelectorAll('.display');
+let tripsBtn = document.querySelector('.extend-btn');
 
-
-    (function(){
-        let changeImg = function(){
-        let min = 10 , max = 60;
-            for(var i = 0; i < displayImg.length; i++){
-                  displayImg[i].style.flex = (Math.floor(Math.random() * (max - min + 1)) + min) + 'vh'; 
-                  displayImg[i].style.flex = (Math.floor(Math.random() * (max - min + 1)) + min) + 'vh';
-                  displayImg[i].classList.toggle('newDisplay');
-            }
+//change images size
+    (() => {
+        let changeSize = () =>{
+            let min = 10 , max = 60;
+                Array.from(displayImg).forEach(image => {
+                    image.style.flex = (Math.floor(Math.random() * (max - min + 1)) + min) + 'vh';
+                    
+                });
         };
-        
-        button.addEventListener("click", changeImg);  
+        button.addEventListener("click", changeSize);  
+        changeSize();
+    })();
+//change img     
+    (() => {
+        let changeImg = () => {
+            Array.from(displayImg).forEach(image => {
+                 newPhoto.addEventListener('click', () => image.classList.add('newDisplay'));  
+                  setTimeout(() =>{
+                   image.classList.remove('newDisplay');
+                },500);
+            });
+        };    
         changeImg();
+    })();
+//toggle nav trips    
+    (() => {
+            let trips = document.querySelector('.trips');
+            tripsBtn.addEventListener("click", () => {
+                trips.classList.toggle('trips-display');
+            });
     })();
 
 
+
+//fetch data from google photos
 /*button.addEventListener("click", function(){
     console.log('click');
       axios.get(url)
